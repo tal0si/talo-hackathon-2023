814debd043a7ca9bec7e03fd61fe34ef60d013a8
arthurder <arthurderosny@gmail.com>
2023-11-25 14:19:17 +0100

    [*] tests CICD

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -11,7 +11,9 @@ if __name__ == "__main__":
         sys.exit(1)
 
     string1 = sys.argv[1]
+    print(string1)
     string2 = sys.argv[2]
+    print(string2)
 
     result = compare_strings(string1, string2)
 
