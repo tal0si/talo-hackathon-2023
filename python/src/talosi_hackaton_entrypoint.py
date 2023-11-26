a3e2c769ce1d6e6b9c67521a7fd3b29366f620f1
Noe <noe.pamula@gmail.com>
2023-11-26 18:05:16 +0100

    :recycle:

---
+++ python/src/talosi_hackaton_entrypoint.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+class TalosiHackatonEntrypoint:
+    def __init__(self):
+        self.connection = psycopg2.connect(dbname="postgres", user="postgres", password="postgres", host="database")
+        self.cursor = self.connection.cursor()
+
+    def run(self):
+        self.print_hello_value()
+        self.close()
+
+    def print_hello_value(self):
+        sql = "SELECT hello FROM hello_world"
+        self.cursor.execute(sql)
+        hello_value = self.cursor.fetchone()[0]
+        print(hello_value)
+
+    def close(self):
+        self.cursor.close()
+        self.connection.close()
