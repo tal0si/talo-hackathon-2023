dd022e2d051dcf3d4cde63fed9f8498e4f4bc0c4
Sébastien Morel <smorel@norauto.com>
2023-12-04 10:51:20 +0100

    :memo: add questions

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,24 +1,81 @@
 import { helloWorld } from "./cfg/database.js"
 
+/**
+ * Combien y a t il de salarié en date du 2022-08-08 ?
+ */
 function question1()  {
     return ""
 }
+
+/**
+ * Quel est l'âge moyen chez talosi ?
+ *  
+ */
 function question2()  {
     return ""
 }
+
+/**
+ * Combien de mission différentes a fait Michel ?
+ *  
+ */
 function question3()  {
     return ""
 }
+
+/**
+ * Combien de personnes a coopté Winnie ?
+ *  
+ */
 function question4()  {
     return ""
 }
+
+/**
+ * Quel est le repas d’Eddy le dimanche 28 septembre 2023 ?
+ *  
+ */
 function question5()  {
     return ""
 }
+
+/**
+ * Pourquoi Kevin met une chemise bleue ?
+ *  
+ */
 function question6()  {
     return ""
 }
+
+/**
+ * Calculer la somme des primes de cooptation de michel sur les mois de novembre 2023.
+ *  
+ */
 function question7()  {
+    return ""
+}
+
+/**
+ * Calculer la somme des primes de mission de Michel sur les mois de  novembre 2023.
+ *  
+ */
+function question8()  {
+    return ""
+}
+
+/**
+ * Calculer la somme des primes de tous les talosiens sur le mois de décembre 2023.
+ *  
+ */
+function question9()  {
+    return ""
+}
+
+/**
+ * Créer une MR avec mon code et des tests Unitaire.
+ *  
+ */
+function question10()  {
     return ""
 }
 
@@ -32,4 +89,7 @@ console.log("question 4 :",question4())
 console.log("question 5 :",question5())
 console.log("question 6 :",question6())
 console.log("question 7 :",question7())
+console.log("question 8 :",question8())
+console.log("question 9 :",question9())
+console.log("question 10 :",question10())
 console.log("======================================================================")
